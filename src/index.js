1e75d93d456baa9df0fd9498efb6fcf516b86756
A.J <ali.jamalzad@gmail.com>
2023-04-22 22:46:50 +0330

    feat: added more methods

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ class Pubq {
     constructor(applicationKey) {
         this.applicationKey = applicationKey;
         this.socket = null;
-        this.connect();
+        this.create();
     }
 
-    connect() {
+    create() {
         this.socket = socketClusterClient.create({
             hostname: process.env.PUBQ_PUBSUB_HOSTNAME,
             secure: process.env.PUBQ_PUBSUB_SECURE,
@@ -33,12 +33,85 @@ class Pubq {
         })();
     }
 
-    subscribe(channel) {
-        return this.socket.subscribe(`${this.applicationKey}/${channel}`);
+    connect() {
+        return this.socket.connect();
     }
 
     disconnect() {
-        this.socket.disconnect();
+        return this.socket.disconnect();
+    }
+
+    getState() {
+        return this.socket.getState();
+    }
+
+    isAuthenticated() {
+        return this.socket.isAuthenticated();
+    }
+
+    deauthenticate() {
+        return this.socket.deauthenticate();
+    }
+
+    subscribe(channelName) {
+        return this.socket.subscribe(`${this.applicationKey}/${channelName}`);
+    }
+
+    unsubscribe(channelName) {
+        return this.socket.unsubscribe(`${this.applicationKey}/${channelName}`);
+    }
+
+    subscriptions(includePending = false) {
+        return this.socket.subscriptions(includePending);
+    }
+
+    isSubscribed(channelName, includePending = false) {
+        return this.socket.isSubscribed(
+            `${this.applicationKey}/${channelName}`,
+            includePending
+        );
+    }
+
+    channel(channelName) {
+        return this.socket.channel(`${this.applicationKey}/${channelName}`);
+    }
+
+    closeChannel(channelName) {
+        return this.socket.closeChannel(
+            `${this.applicationKey}/${channelName}`
+        );
+    }
+
+    closeAllChannels() {
+        return this.socket.closeAllChannels();
+    }
+
+    killChannel(channelName) {
+        return this.socket.killChannel(`${this.applicationKey}/${channelName}`);
+    }
+
+    killAllChannels() {
+        return this.socket.killAllChannels();
+    }
+
+    listener(eventName) {
+        return this.socket.listener(eventName);
+    }
+
+    closeListener(eventName) {
+        return this.socket.closeListener(eventName);
+    }
+
+    closeAllListeners() {
+        return this.socket.closeAllListeners();
+    }
+
+    killListener(eventName) {
+        return this.socket.killListener(eventName);
+    }
+
+    killAllListeners() {
+        return this.socket.killAllListeners();
     }
 }
 
