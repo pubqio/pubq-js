107d409baa98e333a2cce5320306cfd160aa2ed0
A.J <ali.jamalzad@gmail.com>
2023-11-20 21:40:21 +0330

    feat: added default private options

---
+++ src/defaults/defaultPrivateOptions.ts
@@ -0,0 +1,8 @@
+import { privateOptions } from "../types/privateOptions";
+
+export const defaultPrivateOptions: privateOptions = {
+    hostname: "realtime.pubq.io",
+    secure: true,
+    port: 443,
+    path: "/",
+};
