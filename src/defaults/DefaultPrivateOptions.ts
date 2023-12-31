e7efb2be13de2362af2145bf70c540f98bc4c757
A.J <ali.jamalzad@gmail.com>
2023-12-31 22:00:55 +0330

    fix: renamed default private options file and const

---
+++ src/defaults/DefaultPrivateOptions.ts
@@ -0,0 +1,8 @@
+import { PrivateOptions } from "../types/PrivateOptions";
+
+export const DefaultPrivateOptions: PrivateOptions = {
+    hostname: "realtime.pubq.io",
+    secure: true,
+    port: 443,
+    path: "/",
+};
--- src/defaults/defaultPrivateOptions.ts
+++
@@ -1,8 +0,0 @@
-import { privateOptions } from "../types/privateOptions";
-
-export const defaultPrivateOptions: privateOptions = {
-    hostname: "realtime.pubq.io",
-    secure: true,
-    port: 443,
-    path: "/",
-};
