1a1d9cabc8ccaaa3de1d69a7fe4312d33e98f422
A.J <ali.jamalzad@gmail.com>
2023-12-31 22:10:08 +0330

    feat: added app manager

---
+++ src/App.ts
@@ -0,0 +1,53 @@
+import { Auth } from "./Auth";
+import { CommonOptions } from "./types/CommonOptions";
+import { getJwtPayload, getSignedAuthToken } from "./utils/jwt";
+
+class App {
+    private static instance: App;
+
+    private id: string | undefined;
+
+    constructor() {}
+
+    public static getInstance(): App {
+        if (!this.instance) {
+            this.instance = new App();
+        }
+
+        return this.instance;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    setId(id: string) {
+        this.id = id;
+    }
+
+    extractAndSetId(publicKey: string) {
+        const [appId] = publicKey.split(".");
+        this.setId(appId);
+
+        return this.id;
+    }
+
+    handleAppId(options: CommonOptions, auth: Auth) {
+        if (typeof this.getId() === "undefined") {
+            const authMethod = auth.getAuthMethod();
+
+            if (authMethod === "Bearer") {
+                const token = getSignedAuthToken(options.authTokenName);
+                const payload = getJwtPayload(token);
+                this.extractAndSetId(payload.sub);
+            } else if (
+                authMethod === "Basic" &&
+                typeof options.key !== "undefined"
+            ) {
+                this.extractAndSetId(options.key);
+            }
+        }
+    }
+}
+
+export { App };
