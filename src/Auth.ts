8463279415a4f1451ff6d5289d508bdd1754d7f6
A.J <ali.jamalzad@gmail.com>
2023-12-31 22:09:39 +0330

    refactor: moved auth-related methods from rest to auth class

---
+++ src/Auth.ts
@@ -0,0 +1,215 @@
+import { CommonOptions } from "./types/CommonOptions";
+import { getJwtPayload, getSignedAuthToken } from "./utils/jwt";
+import { getRemainingSeconds } from "./utils/time";
+import { Http } from "./Http";
+import { WebSocket } from "./WebSocket";
+
+class Auth {
+    private options: CommonOptions;
+
+    private http;
+
+    private client;
+
+    private ws = WebSocket.getInstance();
+
+    private refreshTokenIntervalId: any;
+
+    constructor(options: CommonOptions) {
+        this.options = options;
+
+        this.http = new Http();
+
+        this.client = this.http.getClient();
+    }
+
+    getAuthMethod() {
+        if (
+            typeof this.options.authUrl !== "undefined" &&
+            this.options.authUrl
+        ) {
+            return "Bearer";
+        } else if (
+            typeof this.options.key !== "undefined" &&
+            this.options.key
+        ) {
+            return "Basic";
+        }
+
+        return false;
+    }
+
+    private getKeyOrToken() {
+        if (!this.options.authTokenName) {
+            throw new Error("Auth token name can not be empty.");
+        }
+
+        if (this.options.authUrl) {
+            return getSignedAuthToken(this.options.authTokenName);
+        } else if (this.options.key) {
+            return this.getKeyBase64();
+        }
+
+        return false;
+    }
+
+    getKey() {
+        if (this.options.key) {
+            return this.options.key;
+        }
+
+        throw new Error("API key has not been specified.");
+    }
+
+    getKeyBase64() {
+        return Buffer.from(this.getKey()).toString("base64");
+    }
+
+    makeAuthorizationHeader() {
+        if (this.getAuthMethod() && this.getKeyOrToken()) {
+            return `${this.getAuthMethod()} ${this.getKeyOrToken()}`;
+        }
+
+        throw new Error("Auth method has not been specified.");
+    }
+
+    isAuthenticated() {
+        return this.ws.socket.isAuthenticated();
+    }
+
+    basicAuth() {
+        this.ws.socket.invoke("#basicAuth", {
+            key: this.getKey(),
+        });
+    }
+
+    async authenticate(body: object = {}, headers: object = {}) {
+        const authMethod = this.getAuthMethod();
+
+        if (authMethod === "Basic") {
+            this.basicAuth();
+        } else if (authMethod === "Bearer") {
+            const tokenData = await this.requestToken();
+            this.ws.socket.authenticate(tokenData.token);
+        }
+    }
+
+    deauthenticate() {
+        // this.requestRevoke();
+        return this.ws.socket.deauthenticate();
+    }
+
+    async requestToken() {
+        if (this.options.authUrl) {
+            try {
+                const response = await this.client.post(
+                    this.options.authUrl,
+                    this.options.authBody,
+                    { headers: this.options.authHeaders }
+                );
+
+                localStorage.setItem(
+                    this.options.authTokenName,
+                    response.data.data.token
+                );
+
+                return response.data.data;
+            } catch (error) {
+                console.error("Error in requestToken:", error);
+                throw error;
+            }
+        }
+
+        throw new Error("Auth URL has not been provided.");
+    }
+
+    async requestRefresh() {
+        if (this.options.refreshUrl) {
+            try {
+                const body = {
+                    ...this.options.authBody,
+                    ...{
+                        token: getSignedAuthToken(this.options.authTokenName),
+                    },
+                };
+
+                const response = await this.client.post(
+                    this.options.refreshUrl,
+                    body,
+                    {
+                        headers: this.options.authHeaders,
+                    }
+                );
+
+                localStorage.setItem(
+                    this.options.authTokenName,
+                    response.data.data.token
+                );
+
+                return response.data.data;
+            } catch (error) {
+                console.error("Error in requestRefresh:", error);
+                throw error;
+            }
+        }
+
+        throw new Error("Refresh URL has not been provided.");
+    }
+
+    async requestRevoke() {
+        if (this.options.revokeUrl) {
+            try {
+                const body = {
+                    ...this.options.authBody,
+                    ...{
+                        token: getSignedAuthToken(this.options.authTokenName),
+                    },
+                };
+
+                const response = await this.client.post(
+                    this.options.revokeUrl,
+                    body,
+                    {
+                        headers: this.options.authHeaders,
+                    }
+                );
+
+                localStorage.removeItem(this.options.authTokenName);
+
+                return response.data.data;
+            } catch (error) {
+                console.error("Error in requestRevoke:", error);
+                throw error;
+            }
+        }
+
+        throw new Error("Revoke URL has not been provided.");
+    }
+
+    startRefreshTokenInterval() {
+        if (this.getAuthMethod() === "Bearer") {
+            // Stop if any refresh token interval is exist
+            this.stopRefreshTokenInterval();
+
+            this.refreshTokenIntervalId = setInterval(() => {
+                const token = getSignedAuthToken(this.options.authTokenName);
+                const authToken = getJwtPayload(token);
+
+                if (authToken) {
+                    const remainingSeconds = getRemainingSeconds(authToken.exp);
+                    if (remainingSeconds <= 60) {
+                        this.requestRefresh();
+                    }
+                }
+            }, this.options.refreshTokenInterval);
+        }
+    }
+
+    stopRefreshTokenInterval() {
+        if (this.refreshTokenIntervalId) {
+            clearInterval(this.refreshTokenIntervalId);
+        }
+    }
+}
+
+export { Auth };
---
+++ src/REST.ts
@@ -0,0 +1,96 @@
+import { CommonOptions } from "./types/CommonOptions";
+import { DefaultCommonOptions } from "./defaults/DefaultCommonOptions";
+import { Http } from "./Http";
+import { Auth } from "./Auth";
+import { GenerateTokenOptions } from "./types/GenerateTokenOptions";
+
+export namespace Pubq {
+    export class REST {
+        private options: CommonOptions;
+
+        private http;
+
+        private client;
+
+        private version = "v1";
+
+        public auth;
+
+        constructor(options: Partial<CommonOptions>, auth?: Auth) {
+            this.options = { ...DefaultCommonOptions, ...options };
+
+            this.http = new Http();
+
+            this.client = this.http.getClient();
+
+            if (typeof auth === "undefined") {
+                this.auth = new Auth(this.options);
+            } else {
+                this.auth = auth;
+            }
+
+            if (this.options.autoRefreshToken) {
+                this.auth.startRefreshTokenInterval();
+            }
+        }
+
+        async publish(channel: string, data: string | any[]): Promise<any> {
+            const response = await this.client.post(
+                `/${this.version}/channels/messages`,
+                {
+                    channel,
+                    data,
+                },
+                {
+                    headers: {
+                        Authorization: this.auth.makeAuthorizationHeader(),
+                    },
+                }
+            );
+
+            return response;
+        }
+
+        async generateToken(options: GenerateTokenOptions): Promise<any> {
+            const response = await this.client.post(
+                `/${this.version}/keys/tokens`,
+                { clientId: options.clientId },
+                {
+                    headers: {
+                        Authorization: this.auth.makeAuthorizationHeader(),
+                    },
+                }
+            );
+
+            return response;
+        }
+
+        async refreshToken(token: string): Promise<any> {
+            const response = await this.client.post(
+                `/${this.version}/keys/tokens/refresh`,
+                {},
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+
+            return response;
+        }
+
+        async revokeToken(token: string): Promise<any> {
+            const response = await this.client.post(
+                `/${this.version}/keys/tokens/revoke`,
+                {},
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+
+            return response;
+        }
+    }
+}
