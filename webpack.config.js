8e7ef4e7c22cadc632103e6d5892fc1ca81dbcf6
A.J <ali.jamalzad@gmail.com>
2024-01-11 20:49:55 +0330

    feat: build multi-platform

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,35 @@
 const path = require("path");
 
-module.exports = {
-    entry: {
-        pubq: "./src/pubq.ts",
-    },
-    output: {
-        path: path.resolve(__dirname, "bundle"),
-        filename: "[name].js",
-        library: "PUBQ JavaScript SDK",
-        libraryTarget: "umd",
-        globalObject: "this",
-    },
-    mode: "production",
-    resolve: {
-        extensions: [".ts", ".js"],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                exclude: /node_modules/,
-                use: "ts-loader",
-            },
-        ],
-    },
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === "production";
+
+    return {
+        entry: {
+            pubq: "./src/pubq.ts",
+        },
+        output: {
+            path: path.resolve(__dirname, "build/web"),
+            filename: isProduction ? "pubq.min.js" : "pubq.js",
+            library: "PUBQ",
+            libraryTarget: "umd",
+            globalObject: "this",
+        },
+        mode: isProduction ? "production" : "development",
+        devtool: isProduction ? "source-map" : "eval-source-map",
+        resolve: {
+            extensions: [".ts", ".js"],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    exclude: /node_modules/,
+                    use: "ts-loader",
+                },
+            ],
+        },
+        optimization: {
+            minimize: isProduction,
+        },
+    };
 };
