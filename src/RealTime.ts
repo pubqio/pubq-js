963b963cc6bf25eb826e20b8caaa76cbec427b65
A.J <ali.jamalzad@gmail.com>
2024-02-26 00:42:20 +0330

    fix: added destroy and updateOptions methods to RealTime

--- src/RealTime.ts
+++ src/RealTime.ts
@@ -4,16 +4,19 @@ import { DefaultPrivateOptions } from "./defaults/DefaultPrivateOptions";
 import { Auth } from "./Auth";
 import { Connection } from "./Connection";
 import { RealTimeChannels } from "./RealTimeChannels";
+import { App } from "./App";
 
 export namespace Pubq {
     export class RealTime {
-        private options: CommonOptions;
+        public options: CommonOptions;
 
-        public auth;
+        public auth: any;
 
-        public connection;
+        public connection: any;
 
-        public channels;
+        public channels: any;
+
+        public app: any;
 
         constructor(options: Partial<CommonOptions>) {
             this.options = {
@@ -27,6 +30,30 @@ export namespace Pubq {
             this.connection = new Connection(this.options, this.auth);
 
             this.channels = new RealTimeChannels(this.options);
+
+            this.app = App.getInstance();
+        }
+
+        updateOptions(options: Partial<CommonOptions>) {
+            this.options = {
+                ...this.options,
+                ...options,
+            };
+
+            this.auth = new Auth(this.options);
+
+            this.connection = new Connection(this.options, this.auth);
+
+            this.channels = new RealTimeChannels(this.options);
+
+            this.app = App.getInstance();
+        }
+
+        destroy() {
+            this.auth.destroy();
+            this.connection.destroy();
+            this.channels.destroy();
+            this.app.destroy();
         }
     }
 }
