1dd7494a930decaa474447347d31896ed598634a
A.J <ali.jamalzad@gmail.com>
2024-11-04 21:24:32 +0330

    feat: implemented rest

--- src/core/rest.ts
+++ src/core/rest.ts
@@ -1,3 +1,17 @@
+import { Option } from "interfaces/option.interface";
+import { OptionManager } from "./option-manager";
+import { uuidv7 } from "utils/uuid";
+
 export class Rest {
-    constructor() {}
+    private instanceId: string;
+    public optionManager: OptionManager;
+
+    constructor(option?: Partial<Option>) {
+        this.instanceId = `rest_${uuidv7()}`;
+        this.optionManager = OptionManager.getInstance(this.instanceId, option);
+    }
+
+    public reset(): void {
+        this.optionManager.reset();
+    }
 }
